11f8160dac755d6a80b97ec5cd8de8eb80dcc89e
Щербак Алексей <aleksej.shherbak@inbox.ru>
2022-05-28 01:10:41 +0300

    Update menu

--- FrontendApp/my-family-tree/src/App.tsx
+++ FrontendApp/my-family-tree/src/App.tsx
@@ -1,90 +1,96 @@
-import React from 'react';
-import {Box, Button, Divider, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText, List} from "@mui/material";
+import * as React from 'react';
+import {useTheme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import {Drawer} from "@mui/material";
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
+export default function App() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
 
-type Anchor = 'top' | 'left' | 'bottom' | 'right';
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
 
-function InboxIcon() {
-    return null;
-}
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
-function MailIcon() {
-    return null;
-}
+    const menuItems = [
+        {
+            title: 'Tree',
+            icon: <AccountTreeIcon/>},
+        {
+            title: 'Account settings',
+            icon: <ManageAccountsIcon/>
+        }
+    ].map((item) => (
+        <ListItem key={item.title} disablePadding sx={{display: 'block'}}>
+            <ListItemButton>
+                <ListItemIcon
+                    sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : 'auto',
+                        justifyContent: 'center',
+                    }}
+                >
+                    {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.title} sx={{opacity: open ? 1 : 0}}/>
+            </ListItemButton>
+        </ListItem>
+    ));
+    
+    return (
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/> 
+            <AppBar>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && {display: 'none'}),
+                        }}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        MY FAMILY TREE
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer open={open}>
+                <div>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon/> : <ChevronLeftIcon/>}
+                    </IconButton>
+                </div>
+                <Divider/>
+                <List>
+                    {menuItems}
+                </List>
+            </Drawer>
+            <Box component="main" sx={{flexGrow: 1, p: 3}}>
 
-function App() {
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
-
-    const toggleDrawer =
-        (anchor: Anchor, open: boolean) =>
-            (event: React.KeyboardEvent | React.MouseEvent) => {
-                if (
-                    event.type === 'keydown' &&
-                    ((event as React.KeyboardEvent).key === 'Tab' ||
-                        (event as React.KeyboardEvent).key === 'Shift')
-                ) {
-                    return;
-                }
-
-                setState({ ...state, [anchor]: open });
-            };
-
-    const list = (anchor: Anchor) => (
-        <Box
-            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+            </Box>
         </Box>
     );
-
-    return (
-        <div>
-            {(['left', 'right', 'top', 'bottom'] as const).map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                    <Drawer
-                        anchor={anchor}
-                        open={state[anchor]}
-                        onClose={toggleDrawer(anchor, false)}
-                    >
-                        {list(anchor)}
-                    </Drawer>
-                </React.Fragment>
-            ))}
-        </div>
-    );
 }
-
-export default App;
