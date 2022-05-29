16e46211c18ee951fa2f98cd098ece0d5e7353d7
Щербак Алексей <aleksej.shherbak@inbox.ru>
2022-05-29 21:46:46 +0300

    Implemented login form

--- FrontendApp/my-family-tree/src/components/Forms/LoginForm/LoginForm.tsx
+++ FrontendApp/my-family-tree/src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,12 +1,43 @@
-import React, {useState} from 'react';
-import {FormGroup, TextField} from "@mui/material";
+import React, {useEffect, useState} from 'react';
+import {Button, FormGroup, TextField} from "@mui/material";
 import {useTheme} from "@mui/material/styles";
-
+import LoginRequest from "../../../models/Auth/LoginRequest";
+import validator from 'validator'
+ 
 const LoginForm = () => {
     const theme = useTheme();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [isEmailHasCorrectFormat, setIsEmailHasCorrectFormat] = useState<boolean | null>(null);
+    const [isSubmitEnabled, setSubmitEnabled] = useState(false);
+    const [loginRequest, setLoginRequest] = useState<LoginRequest>({
+        email: '',
+        password: ''
+    });
+
+    useEffect(() => {
+        setSubmitAvailability(loginRequest);
+    }, [loginRequest])
+
+    const setSubmitAvailability = (request: LoginRequest) => {
+        if (request.email && request.password && isEmailValid(request.email)) {
+            setSubmitEnabled(true);
+        } else {
+            setSubmitEnabled(false);
+        }
+    }
+
+    const isEmailValid = (email: string): boolean => {
+        if (!validator.isEmail(email)) {
+            setIsEmailHasCorrectFormat(false);
+            return false;
+        }
+        setIsEmailHasCorrectFormat(true);
+        return true;
+    };
     
+    const handleSubmit = async () => {
+        
+    }
+
     return (
         <FormGroup sx={{
             width: theme.spacing(60)
@@ -18,9 +49,12 @@ const LoginForm = () => {
                        sx={{
                            paddingBottom: theme.spacing(2)
                        }}
-                       onChange={(e) => setEmail(e.target.value)}
-                       
+                       onChange={(e) => setLoginRequest({...loginRequest, email: e.target.value})}
+                       required={true}
+                       onBlur={(e) => isEmailValid(e.target.value)}
+                       error={isEmailHasCorrectFormat === false}
             />
+
             <TextField type="password"
                        placeholder="Enter password"
                        label="Password"
@@ -28,8 +62,11 @@ const LoginForm = () => {
                        sx={{
                            paddingBottom: theme.spacing(2)
                        }}
-                       onChange={(e) => setPassword(e.target.value)}
+                       onChange={(e) => setLoginRequest({...loginRequest, password: e.target.value})}
+                       required={true}
             />
+            
+            <Button disabled={!isSubmitEnabled} onClick={handleSubmit} sx={{width: theme.spacing(10)}} variant="contained">Login</Button>
         </FormGroup>
     );
 };
