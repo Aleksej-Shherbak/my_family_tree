fbc795ae7a1893f5cea9929fad5d2c19d91c75cd
Щербак Алексей <aleksej.shherbak@inbox.ru>
2022-06-25 13:49:28 +0300

    Updated CreateTreeForm.tsx

--- FrontendApp/my-family-tree/src/components/Forms/CreateTreeForm.tsx
+++ FrontendApp/my-family-tree/src/components/Forms/CreateTreeForm.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import {useFormik} from 'formik';
+import {Formik, useFormik} from 'formik';
 import * as yup from 'yup';
-import {Button, TextField} from "@mui/material";
+import {Button, Input, TextField} from "@mui/material";
+import {useTheme} from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import {PhotoCamera} from "@mui/icons-material";
+import PreviewImage from "./PreviewImage";
 
 const validationSchema = yup.object({
     title: yup
@@ -11,45 +15,83 @@ const validationSchema = yup.object({
 
 interface CreateTreeFormType {
     title: string,
-    description?: string
+    description?: string,
+    image?: File
 }
 
 const CreateTreeForm = () => {
-    const formik = useFormik<CreateTreeFormType>({
-        initialValues: {
-            title: 'My new family tree',
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            console.log(JSON.stringify(values, null, 2));
-        },
-    });
+    const theme = useTheme();
+    const initialValue: CreateTreeFormType = {
+        title: 'My new family tree',
+    };
 
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <TextField
-                    fullWidth
-                    name="title"
-                    label="Title"
-                    type="text"
-                    value={formik.values.title}
-                    onChange={formik.handleChange}
-                    error={formik.touched.title && Boolean(formik.errors.title)}
-                    helperText={formik.touched.title && formik.errors.title}
-                />
-                <TextField
-                    fullWidth
-                    label="Description"
-                    type="text"
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                />
-                <Button color="primary" variant="contained" fullWidth type="submit">
-                    Submit
-                </Button>
-            </form>
-        </div>
+        <Formik
+            initialValues={initialValue}
+            validationSchema={validationSchema}
+            onSubmit={(values, {setSubmitting}) => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+            }}
+        >
+            {({
+                  values, isSubmitting,
+                  errors,
+                  touched,
+                  handleChange,
+                  setFieldValue
+              }) => (
+                <form>
+                    {values.image && <PreviewImage file={values.image}/>}
+                    <label htmlFor="icon-button-file">
+                        <IconButton color="primary" aria-label="upload picture" component="span" sx={{
+                            marginBottom: theme.spacing(2)
+                        }}>
+                            <PhotoCamera sx={{marginRight: theme.spacing(1)}}/> UPLOAD IMAGE
+                        </IconButton>
+                        <Input
+                            name="image"
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                                {setFieldValue('image', e.currentTarget.files?.item(0))}}
+                            id="icon-button-file"
+                            type="file"
+                            sx={{
+                                display: "none"
+                            }}/>
+                    </label>
+
+                    <TextField
+                        fullWidth
+                        name="title"
+                        label="Title"
+                        type="text"
+                        value={values.title}
+                        error={touched.title && Boolean(errors.title)}
+                        helperText={touched.title && errors.title}
+                        onChange={handleChange}
+                        sx={{
+                            marginBottom: theme.spacing(2)
+                        }}
+                    />
+
+                    <TextField
+                        fullWidth
+                        multiline
+                        rows={5}
+                        label="Description"
+                        type="text"
+                        value={values.description}
+                        onChange={handleChange}
+                        sx={{
+                            marginBottom: theme.spacing(2)
+                        }}
+                    />
+                    <Button color="primary" variant="contained" type="submit">
+                        Submit
+                    </Button>
+                </form>
+            )}
+        </Formik>
     );
 };
 
